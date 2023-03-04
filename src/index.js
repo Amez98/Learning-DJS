19cadf1afb8ebec01ff2498fa46f01eeb8bb68a4
Amez98 <112534703+Amez98@users.noreply.github.com>
2023-03-03 21:21:54 -0800

    Added all Discordjs code I have learned to date

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import { config } from 'dotenv';
+import { Client, GatewayIntentBits, Routes } from 'discord.js';
+import { REST } from '@discordjs/rest';
+
+config();
+
+const TOKEN = process.env.BOT_TOKEN;
+const CLIENT_ID = process.env.CLIENT_ID;
+const GUILD_ID = process.env.GUILD_ID;
+
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ],
+});
+
+const rest = new REST({ version: '10' }).setToken(TOKEN);
+
+client.login(TOKEN);
+client.on('ready', () => {console.log(`${client.user.tag} has logged in!`);});
+
+async function main() {
+    const commands = [
+    {
+        name: 'tutorialhelp',
+        description: 'Help Tutorial Command', 
+    },
+    {
+        name: 'tutorialhelp2',
+        description: 'Help Tutorial Command', 
+    },
+];
+
+    try {
+        console.log('Started refreshing application (/) commands.');
+        await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {
+            body: commands,
+        });
+
+        client.login(TOKEN);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+main();
